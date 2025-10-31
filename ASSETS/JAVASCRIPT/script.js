7124862b3a4a1548b38d6fef84a46976511316d6
Miguel Denner <migueldennerrocha@gmail.com>
2025-10-30 22:35:46 -0300

    Update script.js

--- ASSETS/JAVASCRIPT/script.js
+++ ASSETS/JAVASCRIPT/script.js
@@ -9,3 +9,24 @@
 window.alert('Olá Mundo')
 window.confirm('Está gostando da página??')
 window.prompt('Qual é o seu nome??')
+
+/*Variáveis*/
+/*          var, let ou const
+         ""    ,  ''     ,   ``           */
+
+var idade = 18
+var grana = 1518.13
+var nome = Migas
+
+/*OBS: typeof é um comando usado para descobrir o tipo da variável*/
+
+typeof idade
+typeof grana
+typeof nome
+
+/*Guardar dados*/
+
+var nome1  = window.prompt('Qual é o seu nome??')
+window.alert('É um grande prazer te receber' + nome1 + '!')
+
+
